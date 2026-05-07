80fa71c3d6f5517893718d1a035196cb4925206d
zituryoku0 <tommykouki1031@icloud.com>
2026-05-07 21:19:20 +0900

    20260507時点

--- dp/src/bin/c.rs
+++ dp/src/bin/c.rs
@@ -1,3 +1,38 @@
+use proconio::{input, marker::Chars};
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+use std::collections::HashSet;
+use std::collections::VecDeque;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        event: [[i32; 3];n],
+    }
+
+    let mut dp = vec![vec![0; 3]; n];
+
+    for i in 0..n {
+        for j in 0..3 {
+            if i == 0 {
+                dp[i][j] = event[i][j];
+                continue;
+            }
+            for k in 0..3 {
+                // 前日と同じものは選べない
+                if j == k {
+                    continue;
+                }
+                dp[i][j] = dp[i][j].max(dp[i - 1][k] + event[i][j]);
+            }
+        }
+    }
+
+    let mut ans = 0;
+    for i in 0..3 {
+        if ans < dp[n - 1][i] {
+            ans = dp[n - 1][i];
+        }
+    }
+    println!("{}", ans);
 }
